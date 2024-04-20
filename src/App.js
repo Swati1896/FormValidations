6dceadec5d9faf6ec74f7cee767041369c8024f9
Pankaj Bhakre <pankajbhakre11@gmail.com>
2024-04-20 14:05:40 +0530

    Form Validations Changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormValidationExample from './FormValidationExample';
+
+function App() {
+  return (
+    <div className="container">
+      <FormValidationExample/>
+    </div>
+  );
+}
+
+export default App;
